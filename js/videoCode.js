c4f0ff711644a898d252f240b2a40f1f3853e511
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2023-12-04 05:02:42 -0300

    mute/unmute

---
+++ js/videoCode.js
@@ -0,0 +1,12 @@
+document.getElementById('mute-button').addEventListener('click', function() {
+    var video = document.querySelector('.background-clip');
+    video.muted = !video.muted;
+});
+document.getElementById('mute-button').addEventListener('change', function() {
+    var status = document.getElementById('status');
+    if(this.checked) {
+        status.textContent = 'Off';
+    } else {
+        status.textContent = 'On';
+    }
+});
